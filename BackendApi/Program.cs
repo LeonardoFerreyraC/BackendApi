c35ace452aace1efa16f77ca5ea4c8c964a21988
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-06 02:35:52 -0500

    chore(analytics) Cambiando Swagger

--- BackendApi/Program.cs
+++ BackendApi/Program.cs
@@ -62,11 +62,8 @@ using (var context = scope.ServiceProvider.GetService<AppDbContext>())
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
